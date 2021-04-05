122187945dc90930b2637b9358c10e177db360f9
s1lor4k <karolis.nakutavicius1@gmail.com>
2021-04-05 12:30:03 +0300

    Client side authentication

---
+++ PersonalFinanceManager/Client/Contracts/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using PersonalFinanceManager.Shared.Models;
+using PersonalFinanceManager.Shared.Responses;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceManager.Client.Contracts
+{
+    public interface IAuthService
+    {
+        Task<LoginResult> Login(LoginModel loginModel);
+        Task Logout();
+        Task<RegisterResult> Register(RegisterModel registerModel);
+    }
+}
---
+++ PersonalFinanceManager/Client/Pages/RegisterView.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using PersonalFinanceManager.Client.ViewModels;
+using PersonalFinanceManager.Components.ViewModels;
+
+namespace PersonalFinanceManager.Client.Pages
+{
+    public class RegisterViewComponent : ViewModelComponentBase<RegisterViewModel>
+    {
+        [Inject] public override RegisterViewModel ViewModel { get; set; }
+    }
+
+}
